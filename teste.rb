2d01cb28e6d44b6eeff84dd34520970f90cb9f93
Daniel Lima <daniel@macluan.local>
2020-03-06 20:54:32 -0300

    criando repositorio e comitando operação.

---
+++ teste.rb
@@ -0,0 +1,16 @@
+#for numero in (1..10)
+    #puts "Numero: #{numero}"
+#end
+
+#numero = 0
+#while numero <= 5
+    #puts "Numero: #{numero}"
+    #numero += 1
+#end
+
+#sting_especial = %{Luan é "normal" vc' perder' no Clash para me.}
+#puts sting_especial
+
+string_especial = %{Isso é "normal" e {util} no mundo Ruby
+                    e a partir de agora veremos a 'todo' momento}
+puts string_especial

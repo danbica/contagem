f6e49974e81007c86b57227d27dcd4a445139437
Daniel Lima <daniel@macluan.local>
2020-03-08 21:39:04 -0300

    Adicionado uma nova forma de exibição de contagem de 0 a 100.

--- Capitulo01/contagem.rb
+++ Capitulo01/contagem.rb
@@ -1,13 +1,19 @@
-#nome = "Daniel"
-#boas_vindas = "Seja bem-vindo(a) #{nome}"
-#puts boas_vindas
+#Será exibido as mensagem de boas vindas ao usuario de acordo com o nome na variavel.
+nome = "Daniel"
+boas_vindas = "Seja bem-vindo(a) #{nome}"
+puts boas_vindas
 
+#Executará se o individuo for maior que 18 anos.
 idade = 19
 nome = "Lucas"
 
 if idade > 18
     puts nome
 end 
+
+for numero in(1..100)
+    puts "Numero: #{numero}"
+end    
 
 
 
